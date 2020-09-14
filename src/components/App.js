60273c7d3b1cb4d493f14e5f6a189ecdeea1d130
Shalini Gupta <shalini230395@gmail.com>
2020-09-14 21:44:28 +0530

    Add Signin component and .prettierrc

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-
+import React from "react";
+import { SignIn } from "./";
 
 function App() {
   return (
     <div className="App">
       Hello World
+      <SignIn />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { App } from './components';
+import { App, SignIn } from './components';
 
 ReactDOM.render(
   <React.StrictMode>
