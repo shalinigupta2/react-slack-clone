de538e0fc230c92ac21c8f02b00c0e3d9140d2ee
Shalini Gupta <shalini230395@gmail.com>
2020-09-14 22:22:24 +0530

    Signin styling

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-# Creating a new Project
+## Creating a new Project
+
 Make sure you have npm and node and create-react-app too..
 
 ```
@@ -13,7 +14,8 @@ OR
 git clone https://github.com/shalinigupta2/react-slack-clone
 ```
 
-# Starting a project
+## Starting a project
+
 ```
 npm start
 ```
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import { SignIn } from "./";
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-      <SignIn />
-    </div>
-  );
+class App extends Component {
+  state = {
+    counter: 1
+  };
+  render() {
+    return (
+      <div className="App">
+        Hello World
+        <SignIn />
+      </div>
+    );
+  }
 }
 
 export default App;
